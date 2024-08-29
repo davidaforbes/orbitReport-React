5973b0e7614b92220b81e74e69f4d08ddf9e055e
unknown <davidarthurforbes@gmail.com>
2024-08-28 23:32:51 -0400

    Working on buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,19 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
+import satData from "./satData";
+
+const Buttons = ({ filterByType, setSat, orbitTypes}) => {
+  {orbitTypes.map((type, index) => {
+    return (
+      <button onClick={() => filterByType(type)} key={index}>
+        {type} Orbit
+      </button>
+    );
+  })}
+    return (
+      <button onClick={() => setSat(satData)}>
+        All Orbits
+      </button>
+    )
 };
+   
 
 export default Buttons;
